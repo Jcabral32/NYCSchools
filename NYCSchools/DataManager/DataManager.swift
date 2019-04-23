9fa63637afd98924e9bbacb80849e9f0c6e78440
jcabral32 <jcabral18@gmail.com>
2019-04-23 10:04:48 -0400

    Created Folder Structure. Started building the UI. Created Data Manager and commented the API endpoint.

---
+++ NYCSchools/DataManager/DataManager.swift
@@ -0,0 +1,14 @@
+//
+//  DataManager.swift
+//  NYCSchools
+//
+//  Created by Jean Cabral on 4/23/19.
+//  Copyright © 2019 Jean Cabral. All rights reserved.
+//
+
+import Foundation
+// https://data.cityofnewyork.us/resource/s3k6-pzi2.json --> API EndPoint
+class DataManager{
+    
+    
+}
